157073c8a5512d4a9016dc0d7c59ecd9325f926a
GibreelAbdullah <gibreel.khan@gmail.com>
2025-06-09 08:09:26 +0530

    Added Content By Hisham

--- src/lib/components/footer.svelte
+++ src/lib/components/footer.svelte
@@ -4,10 +4,16 @@
 
 <footer class="border-t-[1px] border-surface-500/20 py-4 px-4 xl:px-0 opacity-100">
 	<div class="container mx-auto flex flex-col xl:flex-row justify-between items-center gap-4">
-		<div class="flex items-center gap-4">
-			<!-- <Icon name="skeleton" size={20} /> -->
+		<div class="flex flex-col items-start gap-4">
 			<p class="type-scale-1 opacity-100">
-				Built By <a class="underline" href="https://www.linkedin.com/in/gibreel-abdullah/" target="_blank"
+				Content : <a
+					class="underline"
+					href="https://www.researchgate.net/profile/Muhammed-Hisham"
+					target="_blank">Muhammad Hisham</a
+				>
+				<br />
+				Website :
+				<a class="underline" href="https://www.linkedin.com/in/gibreel-abdullah/" target="_blank"
 					>Gibreel Abdullah</a
 				>
 			</p>
