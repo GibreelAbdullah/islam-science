413fa50fe60b729a9e82a9942deeb7f9eb943e58
GibreelAbdullah <gibreel.khan@gmail.com>
2025-06-20 12:47:29 +0530

    Fixed background

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -9,7 +9,7 @@
 <div
 	aria-hidden="true"
 	class="pointer-events-none absolute inset-0 z-0 opacity-10"
-	style="background-image: url('/islamic-pattern.svg'); background-size: 400px 400px; background-repeat: repeat;"
+	style="background-image: url('/islamic-pattern.svg'); background-size: 400px 400px; background-repeat: repeat; min-height: 100vh; height: 100%; width: 100vw; position: fixed;"
 ></div>
 <div
 	class="min-h-screen bg-gradient-to-br from-surface-50 to-surface-200 dark:from-surface-950 dark:to-surface-900"
